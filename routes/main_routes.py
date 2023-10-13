2d657f7313413a2eaaabf875976fee625b579b4c
JasonPham <phamthaitienthinh@gmail.com>
2023-10-13 13:06:33 -0400

    backend error handling

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -16,19 +16,33 @@ def thankyou():
 def get_weather():
     
     print(os.environ.get("OPENWEATHER_API_KEY"))
-    city = request.args.get('city')
-    
-    #Fetch the API key from the environment variable
+    #Fetch the API key from the environment variable and check to see if the api key is missing
     api_key = os.environ.get("OPENWEATHER_API_KEY")
-
+    
+    #check to see if the api key is missing, backend error handling
+    if not api_key:
+        return jsonify({"error": "API key is missing"}), 500
+    
+    #check to see if the city is missing, backend error handling
+    city = request.args.get('city')
+    if not city:
+        return jsonify({"error": "City input value is missing"}), 400
+    
     #Call the OpenWeather API
     base_url = 'http://api.openweathermap.org/data/2.5/forecast'
     complete_url = f"{base_url}?q={city}&appid={api_key}"
     
-    print(complete_url)
-
+    #Make the API call
     response = requests.get(complete_url)
+    
+    #Backend error handling
+    if response.status_code != 200:
+        return jsonify({"error": "There was an error processing with API call"}), 400
+    
+    #Parse the response
     data = response.json()
+    if 'list' not in data:
+        return jsonify({"error": "Invalid data received from OpenWeather API"}), 500
     
     return jsonify(data)
     
